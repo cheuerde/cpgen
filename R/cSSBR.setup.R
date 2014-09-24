c8362f8edbab3e96142eac118c3d97fbd9a384fb
Claas Heuer <cheuer@tierzucht.uni-kiel.de>
2014-09-24 13:53:51 +0200

    Create cSSBR.setup.R
    function that generates model terms for SSBR - useful for multiple phenotypes
    or cross validation

---
+++ R/cSSBR.setup.R
@@ -0,0 +1,128 @@
+#
+# cSSBR.setup.R
+# Claas Heuer, September 2014
+#
+# Copyright (C)  2014 Claas Heuer
+#
+# This file is part of cpgen.
+#
+# cpgen is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# cpgen is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# in file.path(R.home("share"), "licenses").  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
+# cSSBR.setup
+
+cSSBR.setup <- function(data, M, verbose=TRUE) {
+
+
+# double check some stuff to prevent clmm from failing 
+# after having already done some heavy computations
+if(class(data)!="data.frame") stop("'data' has to be of type 'data.frame'") 
+
+if(is.null(data$y)) stop("'data' has to include a phenotype-vector 'y'")
+if(is.null(data$id)) stop("'data' has to include an id-vector 'id'")
+if(is.null(data$sire)) stop("'data' has to include a sire-vector 'sire'")
+if(is.null(data$dam)) stop("'data' has to include a dam-vector 'dam'")
+
+if(class(M)!="matrix") stop("'M' has to be of type 'matrix'") 
+if(is.null(rownames(M))) stop("Please provide rownames for 'M'")
+if(anyNA(M)) stop("No NAs allowed in marker matrix")
+
+# FIXME repeated measures are not implemented yet - find a way not to copy too much
+if(length(unique(data$id))<nrow(data)) stop("Repeated Measures not implmented yet")
+ 
+if(verbose) cat("\n Gathering information and processing pedigree\n")
+
+
+# Constructing pedigree: adding missings and ordering by generation.
+# Individuals with markers might be missing in the data set, so we add those
+# as unrelated founders
+
+data$id <- as.character(data$id)
+data$sire <- as.character(data$sire)
+data$dam <- as.character(data$dam)
+rownames(M) <- as.character(rownames(M))
+
+marker_without_ped <- rownames(M)[!rownames(M)%in%c(data$id,data$sire,data$dam)]
+if(length(marker_without_ped)>0) {
+
+  temp <- data.frame(id = marker_without_ped, sire = NA, dam = NA, y = NA)
+  data <- cbind(data,temp)
+
+}
+
+# R-package: pedigreemm
+ped <- editPed(label=data$id,sire=data$sire,dam=data$dam,verbose=FALSE)
+ped <- pedigreemm::pedigree(label=ped$label,sire=ped$sire,dam=ped$dam)
+
+# construct A-inverse
+Ainv <- getAInv(ped)
+
+# now check which individuals have phenotypes and provide pedigree and/or marker information
+DAT <- data.frame(id=ped@label, y = as.numeric(NA), has_y = 0, has_gt = 0, has_ped = 0, stringsAsFactors=FALSE)
+n = nrow(DAT)
+
+DAT[match(data$id,DAT$id),"y"] <- data[match(DAT[match(data$id,DAT$id),"id"],data$id),"y"]
+DAT[!is.na(DAT$y),"has_y"] <- 1
+DAT[DAT$id%in%rownames(M),"has_gt"] <- 1
+DAT[DAT$id%in%ped@label,"has_ped"] <- 1
+
+ids <- 1:n
+
+genotyped <- ids[match(rownames(M),DAT$id)]
+
+# from the non genotyped we only need those with phenotype
+non_genotyped <- ids[DAT$has_y & !DAT$id%in%DAT[genotyped,"id"]]
+
+# allocate the combined marker matrix for the model (only individuals with phenotype)
+nrow_gt = sum(DAT[genotyped,"has_y"])
+nrow_non_gt = length(non_genotyped)
+tmp <- DAT[genotyped,]
+index_gt <- seq(1,nrow(M))[match(tmp[tmp[,"has_y"]==1,"id"] ,rownames(M))]
+
+if(verbose) cat(" Allocating combined Marker matrix ( n =",nrow_gt + nrow_non_gt,", p =",ncol(M),")\n")
+M_combined <- matrix(double(),nrow = nrow_gt + nrow_non_gt, ncol = ncol(M))
+M_combined[1:nrow_gt,] <- M[index_gt,]
+
+# set up model ids
+model_ids <- c(rownames(M)[index_gt],DAT$id[non_genotyped])
+
+#######################
+### Imputation Step ###
+#######################
+
+if(verbose) cat(" Imputing non-genotyped individuals\n")
+# 'csolve' uses Eigen's 'SimplicalLLT-Solver' which is very fast for this purpose.
+# Now impute the non_genotyped directly into the model matrix
+M_combined[(nrow_gt+1):nrow(M_combined),] <- csolve(Ainv[non_genotyped,non_genotyped],
+(-Ainv[non_genotyped,genotyped]) %c% M)
+
+# obtain the cholesky factor for residual error of non_genotyped
+# using pedigreemm::relfactor
+L <- t(relfactor(ped))
+
+# set zeros for genotyped individuals
+L[match(rownames(M)[index_gt],DAT$id),] <- 0 
+
+# remove individuals we dont need and order
+L <- L[match(model_ids,DAT$id),non_genotyped]
+
+# phenotype vector
+pheno <- DAT$y[match(model_ids,DAT$id)]
+
+return(list(ids = model_ids, y=pheno, Marker_Matrix=M_combined, Z_residual = L))
+
+}
+
+
