54ddca144f08a23d8a41c4eb0ad41ebb81d1de15
Claas Heuer <cheuer@tierzucht.uni-kiel.de>
2015-01-07 11:30:29 +0100

    check 0 variance phenotypes

--- R/clmm.R
+++ R/clmm.R
@@ -49,7 +49,8 @@ if(class(y) == "list") {
     y <- list(y)
     names(y) <- "Phenotype_1"
   }
-#isy <- (1:n)[!is.na(y)]
+  
+if(any(unlist(lapply(Y,function(x)var(x,na.rm=TRUE)))==0)) stop("one or more phenotypes with 0 variance detected")
 
 if(is.null(X)) {
   X = array(1,dim=c(n,1))
